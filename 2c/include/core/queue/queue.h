fc9193d252e6e48b1edb71e615cf31f9e517bfbc
4iwen <luky.bien@gmail.com>
2024-10-10 19:25:44 +0200

    whatever ill just stuff all this into one file

--- 2c/include/core/queue/queue.h
+++ 2c/include/core/queue/queue.h
@@ -1,6 +1,17 @@
 #ifndef TWOC_QUEUE_H
 #define TWOC_QUEUE_H
 
+typedef struct {
+    void *data;
+    size_t size;
+} twoc_queue_item_t;
 
+typedef struct {
+    twoc_queue_item_t *items;
+    size_t size;
+    size_t capacity;
+    size_t head;
+    size_t tail;
+} twoc_queue_t;
 
 #endif // TWOC_QUEUE_H
